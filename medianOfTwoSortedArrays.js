f17e2e0946c6d02106ceec66c5a2a99322ea8125
Mohim129 <countrysidefarm15@gmail.com>
2026-02-10 18:06:48 +0600

    started hard  medianOfTwoSortedArrays

---
+++ medianOfTwoSortedArrays.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+
+const array1 = [1, 3];
+const array2 = [2];
+
+const mergedArray = array1.concat(array2);
+
+console.log(mergedArray);
+mergedArray.sort((a, b) => a - b);
+
+console.log(mergedArray);
