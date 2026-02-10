269a12c3f63ed20e74ea9793d264c71fecab759c
Mohim129 <countrysidefarm15@gmail.com>
2026-02-10 18:37:51 +0600

    findMedianSortedArrays passed

--- medianOfTwoSortedArrays.js
+++ medianOfTwoSortedArrays.js
@@ -1,12 +1,20 @@
-console.log("hello world");
 
+const nums1 = [1, 3, 5, 7, 9];
+const nums2 = [2, 4, 6, 8, 10];
 
-const array1 = [1, 3];
-const array2 = [2];
-
-const mergedArray = array1.concat(array2);
-
-console.log(mergedArray);
-mergedArray.sort((a, b) => a - b);
-
-console.log(mergedArray);
+var findMedianSortedArrays = function (nums1, nums2) {
+    const mergedArray = nums1.concat(nums2);
+    
+    mergedArray.sort((a, b) => a - b);
+    
+    const length = mergedArray.length;
+    
+    if (length % 2 === 0) {
+        const mid1 = length / 2 - 1;
+        const mid2 = length / 2;
+        return (mergedArray[mid1] + mergedArray[mid2]) / 2; 
+    } else {
+        const mid = (length - 1) / 2;
+        return mergedArray[mid];  
+    }
+};
